920aab673aa75a04a1c1b15429d3255408193126
Alex <snarsky93@gmail.com>
2018-09-15 23:31:27 +0300

    первый комит

---
+++ mailer/smart.php
@@ -0,0 +1,42 @@
+<?php 
+
+$user_phone = $_POST['user_phone'];
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'unitech85@mail.ru';                 // Наш логин
+$mail->Password = 'maklaren1410';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('unitech85@mail.ru', 'Александр Владимирович');   // От кого письмо 
+$mail->addAddress('snarsky93@mail.ru');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Новая заявка с сайта';
+$mail->Body    = '
+	Пользователь оставил свои данные <br> 
+	Имя: Неизвестно <br>
+	Телефон: ' . $user_phone . '';
+$mail->AltBody = 'Это альтернативный текст';
+
+if(!$mail->send()) {
+    echo "Error";
+} else {
+		header('location: ../thankyou.html');
+}
+
+?>
